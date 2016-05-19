c85f214ce6bd592a7b298cebd2a52f23310fd6d5
JeremySawh <jeremysawh@gmail.com>
2016-05-19 06:33:09 -0400

    ListView

--- MyApplication/app/src/main/java/com/example/owner/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/owner/myapplication/MainActivity.java
@@ -2,18 +2,52 @@ package com.example.owner.myapplication;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.ScaleDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private android.widget.ListView listview ;
+    private android.widget.ArrayAdapter<String> listAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ////////////////////////////////////
+        // Find the ListView resource.
+        listview = (android.widget.ListView) findViewById( R.id.listview );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        listAdapter = new android.widget.ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+
+        // Add more planets. If you passed a String[] instead of a List<String>
+        // into the ArrayAdapter constructor, you must not add more items.
+        // Otherwise an exception will occur.
+        listAdapter.add( "Ceres" );
+        listAdapter.add( "Pluto" );
+        listAdapter.add( "Haumea" );
+        listAdapter.add( "Makemake" );
+        listAdapter.add( "Eris" );
+        listAdapter.add("X");
+        listAdapter.add("Y");
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        listview.setAdapter( listAdapter );
+
     }
 
 
@@ -24,4 +58,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("testing");
 
     }
+
+
 }
